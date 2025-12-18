1eafc05b40fe2cb8ee5ec3d8471a057a26f1799a
albert11111111 <13370352616@163.com>
2025-12-18 22:20:06 +0800

    lab01 completed

--- Lab/lab01/lab01.py
+++ Lab/lab01/lab01.py
@@ -8,7 +8,7 @@ def digit(n, k):
     >>> digit(3579, 10)
     0
     """
-    return ____
+    return (n // 10**k) % 10
 
 
 def middle(a, b, c):
@@ -26,7 +26,7 @@ def middle(a, b, c):
     >>> middle(30, 5, 40)
     30
     """
-    return ____
+    return a + b + c - min(a, b, c) - max(a, b, c)
 
 
 def falling(n, k):
@@ -41,7 +41,10 @@ def falling(n, k):
     >>> falling(4, 0)
     1
     """
-    "*** YOUR CODE HERE ***"
+    if k == 0:
+        return 1
+    else:
+        return n * falling(n-1, k-1)
 
 
 def divisible_by_k(n, k):
@@ -64,7 +67,13 @@ def divisible_by_k(n, k):
     >>> c
     0
     """
-    "*** YOUR CODE HERE ***"
+    if n < k:
+        return 0
+    else:
+        for i in range(k, n+1):
+                if i % k == 0:
+                    print(i)
+        return n // k
 
 
 def sum_digits(y):
@@ -80,7 +89,10 @@ def sum_digits(y):
     >>> a
     6
     """
-    "*** YOUR CODE HERE ***"
+    if y == 0:
+        return 0
+    else:
+        return y % 10 + sum_digits(y // 10)
 
 
 def double_eights(n):
@@ -98,5 +110,15 @@ def double_eights(n):
     >>> double_eights(80808080)
     False
     """
-    "*** YOUR CODE HERE ***"
+    count = 0
+    while n > 0:
+        if n % 10 == 8:
+            count = count + 1
+            n = n // 10
+        else:
+            count = 0
+            n = n // 10
+        if count == 2:
+            return True
+    return False
 
