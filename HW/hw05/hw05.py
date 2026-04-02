5f2e6945a68d055d84c851b1195b968575147cbf
albert11111111 <13370352616@163.com>
2026-04-02 20:59:08 +0800

    Add initial implementation for hw05, including new files and function updates

--- HW/hw05/hw05.py
+++ HW/hw05/hw05.py
@@ -14,7 +14,7 @@ def hailstone(n):
       if hail[0] == 1:
         return hail[0]
       elif hail[0] % 2 == 0:
-        
+        return hail[0] // 2
 
 
 def merge(a, b):
@@ -35,11 +35,14 @@ def merge(a, b):
     a_val, b_val = next(a), next(b)
     while True:
         if a_val == b_val:
-            "*** YOUR CODE HERE ***"
+          yield a_val
+          a_val, b_val = next(a), next(b)
         elif a_val < b_val:
-            "*** YOUR CODE HERE ***"
+          yield a_val
+          a_val = next(a)
         else:
-            "*** YOUR CODE HERE ***"
+          yield b_val
+          b_val = next(b)
 
 
 def stair_ways(n):
@@ -55,7 +58,15 @@ def stair_ways(n):
     >>> list(s_w) # Ensure you're not yielding extra
     []
     """
-    "*** YOUR CODE HERE ***"
+    if n == 0:
+      yield []
+    elif n == 1:
+      yield [1]
+    else:
+      for way in stair_ways(n - 1):
+        yield [1] + way
+      for way in stair_ways(n - 2):
+        yield [2] + way
 
 
 def yield_paths(t, value):
@@ -94,10 +105,10 @@ def yield_paths(t, value):
     [[0, 2], [0, 2, 1, 2]]
     """
     if label(t) == value:
-        yield ____
+        yield [label(t)]
     for b in branches(t):
-        for ____ in ____:
-            yield ____
+        for x in yield_paths(b, value):
+            yield [label(t)] + x
 
 
 
